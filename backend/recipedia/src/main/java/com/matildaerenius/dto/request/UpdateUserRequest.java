2e1c91730e45310a34cf617af965e698d1e9a8a6
Matilda Erenius <matildaerenius@hotmail.com>
2025-06-30 21:20:06 +0200

    feat: add CustomUserDetailsService and update user endpoint

--- backend/recipedia/src/main/java/com/matildaerenius/dto/request/UpdateUserRequest.java
+++ backend/recipedia/src/main/java/com/matildaerenius/dto/request/UpdateUserRequest.java
@@ -5,8 +5,10 @@ import lombok.Data;
 
 @Data
 public class UpdateUserRequest {
+    private String username;
     private String firstName;
     private String lastName;
     private String address;
+    private String email;
     private Preference preference;
 }
